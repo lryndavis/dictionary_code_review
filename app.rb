8763b28743d288f606a9fee7dad47b9229540461
Lauryn Davis <lauryndavis@tests-MacBook-Pro.local>
2016-01-15 10:40:45 -0800

    pass integration specs with app.rb functionality

--- app.rb
+++ app.rb
@@ -1,1 +1,45 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/word')
+require('./lib/definition')
 
+get('/') do
+  erb(:index)
+end
+
+get('/words/new') do
+  erb(:dictionary_form)
+end
+
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+post('/words') do
+  word_name = params.fetch('word_name')
+  Word.new(:word_name => word_name).save()
+  @words = Word.all()
+  erb(:success)
+end
+
+get('/words/:id') do
+  @words = Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+get('/words/:id/add_definition_form') do
+  @words = Word.find(params.fetch('id').to_i())
+  erb(:add_definition_form)
+end
+
+ post('/words/:id') do
+    @words = Word.find(params.fetch('id').to_i())
+    word_definition = params.fetch('word_definition')
+    @definition = Definitions.new(:word_definition => word_definition)
+    @definition.save()
+    @words.word_meanings.push(@definition)
+    @definitions = Definitions.all()
+    erb(:word)
+  end
---
+++ spec/dictionary_integration_spec.rb
@@ -0,0 +1,50 @@
+require('Capybara/rspec')
+require('./app')
+require('launchy')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+  describe("the word addition patch", {:type => :feature}) do
+    it('adds a word to the dictionary list') do
+      visit('/words/new')
+      fill_in "word_name", :with => "Sherman"
+      click_button("Add")
+      expect(page). to have_content("Success!")
+    end
+  end
+
+describe('the new word list post path', {:type => :feature}) do
+    it('adds a new word and shows the list') do
+       visit('/words/new')
+       fill_in "word_name", :with => "Sherman"
+       click_button("Add")
+       expect(page). to have_content("Success!")
+       click_link('See your Dictionary!')
+       expect(page).to(have_content("Sherman"))
+     end
+   end
+
+   describe('the view a word entry path', {:type => :feature}) do
+     it("shows the word entry of the user's choice") do
+       visit('/words/new')
+       fill_in "word_name", :with => "Sherman"
+       click_button("Add")
+       expect(page). to have_content("Success!")
+       click_link('See your Dictionary!')
+       expect(page).to(have_content("Sherman"))
+       click_link('Sherman', match: :first)
+       expect(page).to(have_content("Sherman"))
+     end
+   end
+
+   describe('the path to add a new definition', {:type => :feature}) do
+     it("will add the definition of a user's choice to selected word") do
+       visit('/words/1')
+       click_link("Add Definition")
+       expect(page).to(have_content("definition"))
+       fill_in "word_definition", :with => "a dog"
+       click_button("Add")
+       expect(page). to have_content("a dog")
+     end
+   end
